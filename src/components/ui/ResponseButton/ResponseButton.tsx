6dcd891a7386e43897ec21ed3214bed61352e107
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-05-22 19:35:05 +0500

    feat: add copy and download buttons

---
+++ src/components/ui/ResponseButton/ResponseButton.tsx
@@ -0,0 +1,18 @@
+import { ClipboardStatic } from '@/components/ui/ClipboardSVG/ClipboardStatic';
+import { ClipboardSuccess } from '@/components/ui/ClipboardSVG/ClipboardSuccess';
+import React from 'react';
+
+interface IProps {
+  children: JSX.Element;
+  onClickFnc: () => void;
+}
+export function ResponseButton(props: IProps) {
+  return (
+    <button
+      className="cursor-pointer border-transparent bg-transparent hover:bg-color-heading-border rounded mr-2"
+      onClick={props.onClickFnc}
+    >
+      {props.children}
+    </button>
+  );
+}
